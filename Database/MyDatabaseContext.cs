ec84571f4488b0a4bafd85954c4225b60f78e6d2
jsld <jotha.solidsnake@gmail.com>
2018-09-09 04:49:28 -0300

    Proyecto base
    +
    Bootstrap

---
+++ Database/MyDatabaseContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Database
+{
+    class MyDatabaseContext
+    {
+        /*
+        public class Person
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+        }
+        public class DatabaseContext : DbContext
+        {
+            public DbSet&lt;Person&gt; Persons { get; set; }
+ 
+            protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            {
+                optionsBuilder.UseSqlite("Data Source=MyDatabase.sqlite");
+            }
+        }
+         */
+    }
+}
