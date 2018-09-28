e6c613306d4441a064f2f2ad77c98854ab4321ca
jsld <jotha.solidsnake@gmail.com>
2018-09-28 04:00:41 -0300

    finalizacion vista empresas

--- FinancialWallet/Views/AddEmpresaView.xaml.cs
+++ FinancialWallet/Views/AddEmpresaView.xaml.cs
@@ -24,5 +24,81 @@ namespace FinancialWallet.Views
         {
             InitializeComponent();
         }
+
+        private void CrearEmpresa_Click(object sender, RoutedEventArgs e)
+        {
+            Border border = new Border
+            {
+                BorderBrush = (SolidColorBrush)new BrushConverter()
+                    .ConvertFrom("#FF252A30"),
+                BorderThickness = new Thickness(1, 1, 1, 1)
+            };
+            Grid grid = new Grid();
+            grid.RowDefinitions.Add(new RowDefinition());
+            grid.ColumnDefinitions.Add(new ColumnDefinition
+            {
+                Width = new GridLength(100)
+            });
+            grid.ColumnDefinitions.Add(new ColumnDefinition
+            {
+                Width = new GridLength(40, GridUnitType.Star)
+            });
+            grid.ColumnDefinitions.Add(new ColumnDefinition
+            {
+                Width = new GridLength(100)
+            });
+            grid.ColumnDefinitions.Add(new ColumnDefinition
+            {
+                Width = new GridLength(80)
+            });
+
+            TextBox idEmpresa = new TextBox
+            {
+                Text = "ID",
+                Name = "IdEmpresa",
+                IsEnabled = false,
+                Margin = new Thickness(10)
+            };
+            Grid.SetColumn(idEmpresa, 0);
+            Grid.SetRow(idEmpresa, 0);
+
+            TextBox nameEmpresa = new TextBox
+            {
+                Text = "Delight Eventos",
+                Name = "NameEmpresa",
+                IsEnabled = false,
+                Margin = new Thickness(10)
+            };
+            Grid.SetColumn(nameEmpresa, 1);
+            Grid.SetRow(nameEmpresa, 0);
+
+            TextBox codeEmpresa = new TextBox
+            {
+                Text = "Code",
+                Name = "CodeEmpresa",
+                IsEnabled = false,
+                Margin = new Thickness(10)
+            };
+            Grid.SetColumn(codeEmpresa, 2);
+            Grid.SetRow(codeEmpresa, 0);
+            
+            Button btnDelete = new Button
+            {
+                Style = (Style)FindResource("DangerButton"),
+                Content = "Borrar",
+                Margin = new Thickness(10)
+            };
+            Grid.SetColumn(btnDelete, 3);
+            Grid.SetRow(btnDelete, 0);
+
+            grid.Children.Add(idEmpresa);
+            grid.Children.Add(nameEmpresa);
+            grid.Children.Add(codeEmpresa);
+            grid.Children.Add(btnDelete);
+
+            border.Child = grid;
+
+            empresasPanel.Children.Add(border);
+        }
     }
 }
